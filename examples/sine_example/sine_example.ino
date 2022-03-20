e9af8fd52af0ea907f3f6b17f556f36664911bba
Giovanni Carbone <gianni.carbone@gmail.com>
2022-03-20 21:06:20 +0100

    updated sine example

--- examples/sine_example/sine_example.ino
+++ examples/sine_example/sine_example.ino
@@ -1,3 +1,17 @@
+// *************************************************
+// example for bluepill SPI1 and MCP 4151
+//  IC        MCU
+//  CS        B9
+//  CLK       A5
+//  DAT       A7
+//  VDD       +5v
+//  VSS       GND
+//  P0B       +5V
+//  P0A       GND
+//  P0W       output signal  
+// ************************************************* 
+
+
 #include "STM32mcp4151.h"
 
 const uint16_t sin_tbl32[32] = {
@@ -7,10 +21,10 @@ const uint16_t sin_tbl32[32] = {
     0,   2,  10,  22,  37,  57,  79, 103
 };
 
-STM32mcp4151 pot(PB9);
+STM32mcp4151 pot(PB9); // default SPI1 will be used. CSpin to PB9. Use: STM32mcp4151 pot(CS_PIN, SPIx) to use different cs pins and SPIs
 
 void runSine32(){
-  // for STM32F103@72MHz. output 1KHz with stock spi driver and 17.5KHz with high perf spi driver
+  // for STM32F103@72MHz. output 878Hz with stock spi driver and 17.5KHz with high perf spi driver
   uint8_t i = 0;
   while(1) {
     pot.wiper(sin_tbl32[i]);
